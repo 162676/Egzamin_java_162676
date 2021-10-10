8dc1e85656d45f2c2ededde7996c3e532f78e3e0
Norbert Rutkowski <162676@student.uwm.edu.pl>
2021-10-11 01:17:02 +0200

    Commit

---
+++ lab0/src/Main.java
@@ -0,0 +1,83 @@
+public class Main {
+
+    public static void main(String[] args) {
+            System.out.println("Zadanie 1");
+            System.out.println(31+29+31);
+
+            System.out.println("Zadanie 2");
+            int liczba=0;
+            for(int i=1;i<=10;i++)
+            {
+                liczba+=i;
+            }
+            System.out.println(liczba);
+
+            System.out.println("Zadanie 3");
+            int licznik=1;
+            for(int i=1;i<=10;i++)
+            {
+               licznik*=i;
+            }
+            System.out.println(licznik);
+
+            System.out.println("Zadanie 4");
+            float kwota1=1000;
+            float procent1 = (kwota1*6)/100;
+            float rok1 = kwota1-procent1;
+            System.out.println("Saldo po 1 roku wynosi " + rok1);
+
+            float kwota2=rok1;
+            float procent2 = (kwota2*6)/100;
+            float rok2 = kwota2-procent2;
+            System.out.println("Saldo po 2 roku wynosi " + rok2);
+
+            float kwota3=rok2;
+            float procent3 = (kwota3*6)/100;
+            float rok3 = kwota3-procent3;
+            System.out.println("Saldo po 3 roku wynosi " + rok3);
+
+            System.out.println("Zadanie 5");
+            System.out.println("+--------+");
+            System.out.println("|  Java  |");
+            System.out.println("+--------+");
+
+            System.out.println("Zadanie 6");
+            System.out.println("  ///// ");
+            System.out.println(" +\"\"\"\"\"+ ");
+            System.out.println("(| 0 0 |)");
+            System.out.println("  | - | ");
+            System.out.println("   (_)    ");
+            System.out.println(" |     | ");
+            System.out.println(" +-----+ ");
+
+            System.out.println("Zadanie 7");
+            System.out.println("**       *                 *                       *        ****             *       *                                     *        ");
+            System.out.println("* *      *                 *                     *****      *   *          *****     *                                     *        ");
+            System.out.println("*  *     *                 *                       *        *   *            *       *  *                                  *  *   * ");
+            System.out.println("*   *    *   *****   * **  *        *****    * **  *        *****            *       * *     *****    *         *  ******  * *      ");
+            System.out.println("*    *   *  *     *  **    *****   *     *   **    *        * *     *     *  *       **     *      *  *         *  *       **     * ");
+            System.out.println("*     *  *  *     *  *     *    *  *******   *     *        *  *    *     *  *       * *    *      *  *    *    *  ******  * *    * ");
+            System.out.println("*      * *  *     *  *     *    *  *      *  *     *    *   *   *   *     *  *    *  *  *   *      *  *   * *   *       *  *  *   * ");
+            System.out.println("*       **   *****   *     *****    ******   *      ****    *    *   *****    ****   *   *   *****     ***   ***   ******  *   *  * ");
+
+            System.out.println("Zadanie 8");
+            System.out.println("   +   ");
+            System.out.println("  + +  ");
+            System.out.println(" +   + ");
+            System.out.println("+-----+");
+            System.out.println("|  _  |");
+            System.out.println("| | | |");
+            System.out.println("+-+-+-+ ");
+
+
+            System.out.println("Zadanie 9");
+
+
+    
+    
+            
+
+
+
+    }
+}
